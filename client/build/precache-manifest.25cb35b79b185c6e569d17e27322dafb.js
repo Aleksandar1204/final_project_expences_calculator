c1258713fd5d706e90c3c2f587a9263bbb0b9b9b
Aleksandar <aleksandar.temkovski@gmail.com>
2020-01-10 21:18:51 +0100

    modified client gitignore

---
+++ client/build/precache-manifest.25cb35b79b185c6e569d17e27322dafb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7e33c24297ef4b1cf43d7864662c2a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8372e8841006c21c3452",
+    "url": "/static/js/2.db0d4070.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.db0d4070.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc3e18f5a20885ad7ae3",
+    "url": "/static/js/main.850bc6d6.chunk.js"
+  },
+  {
+    "revision": "5536b1ffa5673e4eb964",
+    "url": "/static/js/runtime-main.d07f956b.js"
+  }
+]);
