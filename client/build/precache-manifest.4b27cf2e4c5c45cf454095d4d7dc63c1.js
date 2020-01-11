043ae56e8bb862b9047b21181add8aee22a3fbc3
Aleksandar <aleksandar.temkovski@gmail.com>
2020-01-11 19:14:31 +0100

    final_project_expences_calculator

---
+++ client/build/precache-manifest.4b27cf2e4c5c45cf454095d4d7dc63c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4edb5f1191b37abf03966249c1815d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8be0f0f0e9319bcc67e2",
+    "url": "/static/js/2.4eca5e15.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.4eca5e15.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e2225f2a1536dc89984",
+    "url": "/static/js/main.52e17db8.chunk.js"
+  },
+  {
+    "revision": "5536b1ffa5673e4eb964",
+    "url": "/static/js/runtime-main.d07f956b.js"
+  }
+]);
