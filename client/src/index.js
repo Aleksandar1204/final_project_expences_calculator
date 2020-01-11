e9aeaa1eb062efd74ab8a939e7a246fbcc9c645c
Aleksandar <aleksandar.temkovski@gmail.com>
2020-01-11 16:08:42 +0100

    final_project_expences_calculator

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Newproduct from './components/Newproduct/Newproduct'
 import Login from './components/auth/Login'
+import Register from './components/auth/Register'
 
 
 
