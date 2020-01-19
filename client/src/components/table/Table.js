8ab3587fb6376641e3a40697235a966853b75496
Aleksandar <aleksandar.temkovski@gmail.com>
2020-01-20 00:31:09 +0100

    final_project_expences_calculator

--- client/src/components/table/Table.js
+++ client/src/components/table/Table.js
@@ -56,7 +56,7 @@ class Table extends React.Component {
                             </button>
                             </Link> 
                             <Popup trigger={<button  className="btn-danger" title="Delete this product" id="delete" ><FontAwesomeIcon icon={faTrashAlt}/></button>}
-                               on='click' open={this.state.isOpen} onOpen={this.handleOpen} content = {<button onClick={this.handleClose}>click to close</button>}
+                               on='click' open={this.state.isOpen} onOpen={this.handleOpen} 
                             >
                                                    
                                                    <div className="footer">
