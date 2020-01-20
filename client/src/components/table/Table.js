d7da6fcf5e27c603d849a0d0422d9a3b0123f9bd
Aleksandar <aleksandar.temkovski@gmail.com>
2020-01-20 23:41:41 +0100

    final_project_expences_calculator

--- client/src/components/table/Table.js
+++ client/src/components/table/Table.js
@@ -42,7 +42,7 @@ class Table extends React.Component {
 
     showAlert = () => {
         this.setState({
-            showModal:(
+            showModal:
                 <div>
                 <div className="footer">
                     </div>
@@ -55,7 +55,7 @@ class Table extends React.Component {
                                    </div>
                                </div>
                                </div>   
-            )
+            
         })
     }
 
